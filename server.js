a54b0bdcaf3e1e404a19a92f10f11ca6a0bd9bd7
Marcus Ortense <marcus.ortense@concrete.com.br>
2017-03-17 13:31:11 -0300

    fix server

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import path from 'path'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 const app = express()
 
 app.use(express.static(__dirname + '/'))
-app.get('*', (request, response) => response.sendFile('./index.html'))
+app.get('/*', (request, response) => response.sendfile('./index.html'))
 app.listen(port, () => console.log(`server started on port ${port}`))
